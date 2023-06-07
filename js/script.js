3f7418a739f7c04b39827c6827645ff8dcd0b4f4
lucianadm <lucianadm55@hotmail.com>
2023-06-06 21:55:17 -0300

    agrego js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const btnEnviar = document.querySelector('.btn-enviar');
+const categoria = document.querySelector('#categoria');
+const pagar = document.querySelector('.pagar');
+
+btnEnviar.addEventListener('click',()=>{
+    pagar.innerHTML= 600;
+})

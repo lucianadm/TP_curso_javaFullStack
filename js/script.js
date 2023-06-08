f3afe6d616b5c22f00638ff6f236b0d664e9ed32
lucianadm <lucianadm55@hotmail.com>
2023-06-07 23:06:39 -0300

    voy agregando js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
 const btnEnviar = document.querySelector('.btn-enviar');
+const btnBorrar = document.querySelector('.btn-borrar');
 const categoria = document.querySelector('#categoria');
-const pagar = document.querySelector('.pagar');
+const cantidad = document.querySelector('#cantidad');
+const precio = document.querySelector('.precio');
+const miFormulario=document.querySelector('#miForm');
 
 btnEnviar.addEventListener('click',()=>{
-    pagar.innerHTML= 600;
+    console.log(categoria.value);
+    if (cantidad.value==0) {alert("Debe ingresar la cantidad de entradas.");}
+    if (categoria.value==estudiante){paga=40;}
+    if (categoria.value==trainee) { paga=100;}
+    if (categoria.value==junior)  {paga=170;} 
+  
+    precio.textContent=categoria.value*cantidad.value;
+    btnEnviar.disabled = true;
+})
+
+btnBorrar.addEventListener('click',()=>{
+    miFormulario.reset();
+
 })

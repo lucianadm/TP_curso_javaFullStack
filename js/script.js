25f30d6d1c211a7a1f181cdb19cc6585b0736871
lucianadm <lucianadm55@hotmail.com>
2023-06-09 20:31:00 -0300

    arregle funcionalidad de js

--- js/script.js
+++ js/script.js
@@ -2,14 +2,30 @@ const btnEnviar = document.querySelector('.btn-enviar');
 const btnBorrar = document.querySelector('.btn-borrar');
 const categoria = document.querySelector('#categoria');
 const cantidad = document.querySelector('#cantidad');
+const nombre = document.querySelector('#nombre');
+const apellido = document.querySelector('#apellido');
+const email = document.querySelector('#email');
 const precio = document.querySelector('.precio');
 const miFormulario=document.querySelector('#miForm');
 
 
+
+
+function verificaCampos(){
+if (nombre.value==""){
+    alert("Debe ingresar el nombre.");}
+    else if (apellido.value==""){
+        alert("Debe ingresar el apellido.");}
+     else if (email.value==""){
+            alert("Debe ingresar el email.");}
+      else if (cantidad.value==0) {alert("Debe ingresar la cantidad de entradas.");}
+
+}
+
 function calcularPrecio(){
     var desc=1;
     console.log(categoria.value);
-    if (cantidad.value==0) {alert("Debe ingresar la cantidad de entradas.");}
+  
     let vale=200*cantidad.value;
     switch(categoria.value){
         case "regular":
@@ -31,6 +47,7 @@ function calcularPrecio(){
 
 btnEnviar.addEventListener('click',(e)=>{
   e.preventDefault();
+    verificaCampos();
     calcularPrecio();
   })
 
